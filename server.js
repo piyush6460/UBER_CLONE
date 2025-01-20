4a85416e30475e83738b092f219a75293f0a8674
piyush6460 <prpateliya6460@gmail.com>
2025-01-20 06:50:11 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const app = require('./app')
+const port = process.env.PORT || 8001
+
+
+const server = http.createServer(app);
+
+
+server.listen(port, () => {
+    console.log(`Server is running in :  ${port}`);
+})
